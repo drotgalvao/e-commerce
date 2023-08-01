edefe13466bb42a33153ba7cd0729455f20a2f1e
Diego Galvão <diegohenriquegalvao@gmail.com>
2023-08-01 12:36:31 -0300

    Atualização Layout no App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./App.css";
 
-import Layout from "./components/layout";
+import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
